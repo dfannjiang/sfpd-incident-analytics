6ece613167a5dfd3b8ee77344755143b5939c517
domfj <dfannjiang@gmail.com>
2024-06-10 16:33:44 -0700

    backend starts

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,26 @@
-from . import app
+from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+from .routes import router
+from config import TORTOISE_ORM
+
+app = FastAPI()
+
+app.include_router(router)
+
+# register_tortoise(
+#     app,
+#     config=TORTOISE_ORM,
+#     generate_schemas=True,
+#     add_exception_handlers=True,
+# )
+
+@app.on_event("startup")
+async def startup_event():
+    print("Starting up...")
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    print("Shutting down...")
 
 if __name__ == '__main__':
     import uvicorn
