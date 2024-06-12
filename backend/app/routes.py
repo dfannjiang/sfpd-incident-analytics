992131bdac336fc3417e27f61c0351f668925f8a
domfj <dfannjiang@gmail.com>
2024-06-11 17:24:22 -0700

    add hardcoded data + updated route

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,10 +1,22 @@
 from fastapi import APIRouter, HTTPException
 from .models import Neighborhood
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
 
 router = APIRouter()
 
 @router.get('/neighborhoods/{name}')
 async def get_neighborhood(name: str):
+    df = pd.read_parquet('data/data.parquet')
+    df = df[df.Analysis_Neighborhood == name]
+    df['date'] = df.Incident_DT.dt.date
+    count_by_date = df.groupby('date').size()
     if name:
-        return {"incident_counts": {"larceny": 5513, "robbery": 2341}}
+        return {
+            "category_counts": df.Custom_Category.value_counts().to_dict(),
+            "counts_by_hour": df.groupby('hour_of_day').size().to_dict(),
+            "avg_per_day": count_by_date.mean()
+
+        }
     raise HTTPException(status_code=404, detail="Neighborhood not found")
