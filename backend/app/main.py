c2025ee244e6f86339e55814e8b85eab4a1f5e1d
domfj <dfannjiang@gmail.com>
2024-06-16 00:01:43 -0700

    print port

--- backend/app/main.py
+++ backend/app/main.py
@@ -37,4 +37,5 @@ async def shutdown_event():
 if __name__ == '__main__':
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
+    print(f"Running on port {port}")
     uvicorn.run(app, host='0.0.0.0', port=port)
