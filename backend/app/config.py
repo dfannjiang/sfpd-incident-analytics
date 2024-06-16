cf281c03e35baa833b143a997693a090653b0ced
domfj <dfannjiang@gmail.com>
2024-06-15 19:39:32 -0700

    use load_dotenv in .env file

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,15 +1,17 @@
-from dotenv import dotenv_values
+import os
+from dotenv import load_dotenv
 
 # Load environment variables from .env file into a dictionary
-env_vars = dotenv_values(".env")
+load_dotenv()
 
 # Access the variables
-DB_USERNAME = env_vars["DB_USERNAME"]
-DB_PASSWORD = env_vars["DB_PASSWORD"]
-DB_NAME = env_vars["DB_NAME"]
-DB_PORT = env_vars["DB_PORT"]
-SOCRATA_APP_TOKEN = env_vars["SOCRATA_APP_TOKEN"]
-DB_URL = f"postgres://{DB_USERNAME}:{DB_PASSWORD}@localhost:{DB_PORT}/{DB_NAME}"
+DB_USERNAME = os.environ.get("DB_USERNAME")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_NAME = os.environ.get("DB_NAME")
+DB_PORT = os.environ.get("DB_PORT")
+SOCRATA_APP_TOKEN = os.environ.get("SOCRATA_APP_TOKEN")
+DEV_DB_URL = f"postgres://{DB_USERNAME}:{DB_PASSWORD}@localhost:{DB_PORT}/{DB_NAME}"
+DB_URL = os.environ.get("DATABASE_URL", DEV_DB_URL)
 
 TORTOISE_ORM = {
     "connections": {
