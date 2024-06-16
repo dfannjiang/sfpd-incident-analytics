103f64088eb33413b20667818789067993705cbe
domfj <dfannjiang@gmail.com>
2024-06-15 20:04:29 -0700

    add sodapy to reqs

--- backend/init_tortoise.py
+++ backend/init_tortoise.py
@@ -5,6 +5,7 @@ from app.config import TORTOISE_ORM
 async def init():
     await Tortoise.init(config=TORTOISE_ORM)
     await Tortoise.generate_schemas()
+    await Tortoise.close_connections()
 
 if __name__ == "__main__":
     asyncio.run(init())
