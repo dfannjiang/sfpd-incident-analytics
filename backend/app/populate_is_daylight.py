2025d3b8a8dd71ba4f0d237e047b83bad3f5923a
domfj <dfannjiang@gmail.com>
2024-06-24 12:56:55 -0700

    add try/catch to populate scripts

--- backend/app/populate_is_daylight.py
+++ backend/app/populate_is_daylight.py
@@ -18,15 +18,19 @@ async def populate_is_daylight():
     )
     await Tortoise.generate_schemas()
 
-    records = await IncidentReport.all()
-    for record in records:
-        record.is_daylight = is_daylight(record.incident_datetime)
-    logging.info(f"Updated {len(records)} records. Starting bulk update")
-    start = time.time()
-    await IncidentReport.bulk_update(records,
-                                     fields=["is_daylight"])
-    logging.info(f"Finished bulk updating in {round(time.time() - start, 2)}s")
-    await Tortoise.close_connections()
+    try:
+      records = await IncidentReport.all()
+      for record in records:
+          record.is_daylight = is_daylight(record.incident_datetime)
+      logging.info(f"Updated {len(records)} records. Starting bulk update")
+      start = time.time()
+      await IncidentReport.bulk_update(records,
+                                      fields=["is_daylight"])
+      logging.info(f"Finished bulk updating in {round(time.time() - start, 2)}s")
+    except Exception as e:
+        logging.error(f'Failed with {str(e)}')
+    finally:
+      await Tortoise.close_connections()
 
 if __name__ == "__main__":
     asyncio.run(populate_is_daylight())
--- backend/app/populate_user_friendly_category.py
+++ backend/app/populate_user_friendly_category.py
@@ -16,18 +16,22 @@ async def populate_user_friendly_category():
     )
     await Tortoise.generate_schemas()
 
-    records = await IncidentReport.all()
-    for record in records:
-        record.user_friendly_category = compute_user_friendly_category(
-            record.incident_category,
-            record.incident_description
-        )
-    logging.info(f"Updated {len(records)} records. Starting bulk update")
-    start = time.time()
-    await IncidentReport.bulk_update(records,
-                                     fields=["user_friendly_category"])
-    logging.info(f"Finished bulk updating in {round(time.time() - start, 2)}s")
-    await Tortoise.close_connections()
+    try:
+        records = await IncidentReport.all()
+        for record in records:
+            record.user_friendly_category = compute_user_friendly_category(
+                record.incident_category,
+                record.incident_description
+            )
+        logging.info(f"Updated {len(records)} records. Starting bulk update")
+        start = time.time()
+        await IncidentReport.bulk_update(records,
+                                        fields=["user_friendly_category"])
+        logging.info(f"Finished bulk updating in {round(time.time() - start, 2)}s")
+    except Exception as e:
+        logging.error(f'Failed with {str(e)}')
+    finally:
+        await Tortoise.close_connections()
 
 if __name__ == "__main__":
     asyncio.run(populate_user_friendly_category())
