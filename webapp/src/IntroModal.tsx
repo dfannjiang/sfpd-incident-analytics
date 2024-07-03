8901f224e204b2cf085bf1a198dd55b230129ec9
domfj <dfannjiang@gmail.com>
2024-07-03 15:07:43 -0700

    update intro modal

--- webapp/src/IntroModal.tsx
+++ webapp/src/IntroModal.tsx
@@ -28,6 +28,17 @@ const IntroModal: React.FC<{
           </a>
         </p>
         <p>
+          See{" "}
+          <a
+            href="https://github.com/dfannjiang/sf-city-analytics"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub repo
+          </a>{" "}
+          for more technical details.
+        </p>
+        <p>
           <strong>Important Notice:</strong>
         </p>
         <p>
@@ -38,17 +49,6 @@ const IntroModal: React.FC<{
           Please be aware that the data provided by this application is for
           informational purposes only and should not be used for any official or
           legal purposes.
-        </p>
-        <p>
-          For more technical details, please visit{" "}
-          <a
-            href="https://github.com/dfannjiang/sf-city-analytics"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub repo
-          </a>
-          .
         </p>
       </Modal.Body>
       <Modal.Footer className="bg-dark">
